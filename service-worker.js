2ee39dea52012e4213fc2459e1ea92517c52ca20
Tripn21 <106189682+Tripn21@users.noreply.github.com>
2025-10-28 08:31:51 -0600

    Update service worker cache version and files

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,10 @@
-const CACHE_NAME = 'timeflow-v1';
+const CACHE_NAME = 'timeflow-v2.0';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
